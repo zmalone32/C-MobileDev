7911421376bd4b4de900116eb888bf9e635123e8
zmalone32 <48159980+zmalone32@users.noreply.github.com>
2021-04-02 00:01:47 -0600

    FizzBuzz

---
+++ FizzBuzz/FizzBuzzConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 15 == 0)
+                {
+                   Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 5 == 0)
+                {
+                   Console.WriteLine("Buzz");
+                }
+                else if (i % 3 == 0)
+                {
+                   Console.WriteLine("Fizz");
+                }
+            }
+            
+        }
+    }
+}
