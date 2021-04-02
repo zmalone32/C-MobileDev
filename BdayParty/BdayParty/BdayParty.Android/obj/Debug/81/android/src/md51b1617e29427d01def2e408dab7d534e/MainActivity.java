1a880eb3bbd21a8d79f9940950c88dafb4b14f97
zmalone32 <48159980+zmalone32@users.noreply.github.com>
2021-04-01 23:48:28 -0600

    bdayyy

---
+++ BdayParty/BdayParty/BdayParty.Android/obj/Debug/81/android/src/md51b1617e29427d01def2e408dab7d534e/MainActivity.java
@@ -0,0 +1,47 @@
+package md51b1617e29427d01def2e408dab7d534e;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("BdayParty.Droid.MainActivity, BdayParty.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("BdayParty.Droid.MainActivity, BdayParty.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
